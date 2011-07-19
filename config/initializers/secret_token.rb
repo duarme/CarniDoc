5138dca4e30cbaf61a6f365183810d9e379f6177
Duccio Armenise <info@duccioarmenise.it>
2011-07-19 18:26:26 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Carnidoc::Application.config.secret_token = 'ee0148bcbbfb400cea6ffb16a3059137fe8674b6d227ead1d0c0984603b631779bd353fe8297af9c1e506bd49fa0c140a254adb82572deef63ec1b99825ea790'
